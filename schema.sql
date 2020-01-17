0a7c6e1e133eab7fbf19eed79836a753ed1ad0c9
Tywunda Shivers <tywunda.shivers@gmail.com>
2020-01-17 14:49:54 -0800

    adding modified schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,53 @@
--- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS blogger;
--- Creates the "blogger" database --
-CREATE DATABASE blogger;
+DROP DATABASE IF EXISTS barber_db;
+
+CREATE DATABASE barber_db;
+​
+USE barber_db;
+​
+DROP TABLE IF EXISTS `appointments`;
+DROP TABLE IF EXISTS `services`;
+DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `barbers`;
+​
+​
+CREATE TABLE `barbers` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(255) NOT NULL,
+	`last_name` varchar(255) NOT NULL,
+	`phone_number` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+​
+CREATE TABLE `appointments` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`barber_id` INT NOT NULL,
+	`customer_id` INT NOT NULL,
+	`time` varchar(255) NOT NULL,
+	`date` DATE NOT NULL,
+	`service_id` INT NOT NULL,
+	`location` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+​
+CREATE TABLE `services` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`rate` DECIMAL NOT NULL,
+	PRIMARY KEY (`id`)
+);
+​
+CREATE TABLE `customers` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(255) NOT NULL,
+	`last_name` varchar(255) NOT NULL,
+	`phone_number` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+​
+ALTER TABLE `appointments` ADD CONSTRAINT `appointments_fk0` FOREIGN KEY (`barber_id`) REFERENCES `barbers`(`id`);
+​
+ALTER TABLE `appointments` ADD CONSTRAINT `appointments_fk1` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`);
+​
+ALTER TABLE `appointments` ADD CONSTRAINT `appointments_fk2` FOREIGN KEY (`service_id`) REFERENCES `services`(`id`);
